f8e7ab0a7abc3e03ca7e4cda6d638c7dd0ee40d6
nakul-saini404 <nakul.saini404@gmail.com>
2025-11-03 18:11:37 +0530

    make changes in vite.config to deply website

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/PortFolio_Site/', // must match your GitHub repo name exactly
   plugins: [react()],
 })
